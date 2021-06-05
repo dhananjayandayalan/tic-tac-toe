6dc5f67bdc0703ad33fa2612637ab6d297f9f68c
dhananjayandayalan <dhananjayan.dayalan@gmail.com>
2021-06-05 19:33:20 +0530

    tic tac toe basic

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Game from './Components/Game/Game';
+
+class App extends React.Component {
+  
+  render() {
+    return (
+      <Game />
+    );
+  }
+}
+
+export default App;
